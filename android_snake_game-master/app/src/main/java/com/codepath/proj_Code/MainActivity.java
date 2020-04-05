2ee71d8557a46b730da80aeca81d89f20d438a08
Nick-liu666 <55857483+Nick-liu666@users.noreply.github.com>
2020-04-05 01:06:54 -0600

    add rank botton

--- android_snake_game-master/app/src/main/java/com/codepath/proj_Code/MainActivity.java
+++ android_snake_game-master/app/src/main/java/com/codepath/proj_Code/MainActivity.java
@@ -1,11 +1,10 @@
 package com.codepath.proj_Code;
 
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
-import android.content.Intent;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.codepath.examples.basicsnakegame.R;
 import com.codepath.examples.basicsnakegame.SnakeGameActivity;
@@ -37,6 +36,15 @@ public class MainActivity extends Activity
             public void onClick(View view) {
                 Intent optionsIntent = new Intent(MainActivity.this, OptionsActivity.class);
                 startActivity(optionsIntent);
+            }
+        });
+
+        Button rankBtn = (Button) findViewById(R.id.rankButton);
+        rankBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rankIntent = new Intent(MainActivity.this, RankActivity.class);
+                startActivity(rankIntent);
             }
         });
     }

8167f4083409e77ea00a5fdc8d332473d0903015
Nick-liu666 <55857483+Nick-liu666@users.noreply.github.com>
2020-04-05 16:39:52 -0600

    add activity of rank button to xml file

--- android_snake_game-master/app/src/main/java/com/codepath/proj_Code/RankActivity.java
+++ android_snake_game-master/app/src/main/java/com/codepath/proj_Code/RankActivity.java
@@ -2,16 +2,28 @@ package com.codepath.proj_Code;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 import com.codepath.examples.basicsnakegame.R;
 
-public class RankActivity extends Activity {
-
+public class RankActivity extends Activity implements AdapterView.OnItemSelectedListener {
+    GameSettingsViewModel model;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppTheme);
         setContentView(R.layout.rank_menu);
 
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        model.selectedItem = (String) parent.getItemAtPosition(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        //Antoher Interface callback
     }
 }
